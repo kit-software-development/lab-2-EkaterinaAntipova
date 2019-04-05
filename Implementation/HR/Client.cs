866a2360fbea3c7dd21e881e54411ea32bd3693b
EkaterinaAntipova <ekaterina.antipova.99@mail.ru>
2019-04-05 22:23:00 +0300

    Update Client.cs

--- Implementation/HR/Client.cs
+++ Implementation/HR/Client.cs
@@ -1,12 +1,37 @@
-﻿namespace Practice.HR
+using Practice.HR.Events;
+using System;
+
+namespace Practice.HR
 {
     /// <summary>
     ///     Скрытая реализация представления о клиенте.
     /// </summary>
-    internal class Client : AbstractPerson
+    internal class Client : AbstractPerson, IClient
     {
         /*
          * TODO #4: Реализуйте интерфейс IClient для класса Client
          */
+
+	private float o_discount;
+
+        public float Discount
+        {
+            get
+            {
+                return o_discount;
+            }
+
+            set
+            {
+                var oldDiscount = o_discount;
+                o_discount = value;
+                if (DiscountChange != null)
+                {
+                    DiscountChange.Invoke(this, new ValueChangeEventArgs<float>(oldDiscount));   
+                }
+            }
+        }
+
+        public event EventHandler<ValueChangeEventArgs<float>> DiscountChange;
     }
 }
