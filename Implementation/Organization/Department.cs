eb88a53b43afab9c706542039ab9b0d563c819c1
EkaterinaAntipova <ekaterina.antipova.99@mail.ru>
2019-04-05 22:24:36 +0300

    Update Department.cs

--- Implementation/Organization/Department.cs
+++ Implementation/Organization/Department.cs
@@ -1,12 +1,19 @@
-﻿namespace Practice.Organization
+namespace Practice.Organization
 {
     /// <summary>
     ///     Скрытая реализация представления об отделе предприятия.
     /// </summary>
-    internal struct Department
+    internal struct Department : IDepartment
     {
         /*
          * TODO #2: Реализуйте интерфейс IDepartment для структуры Department
          */
+
+	public string Name { get; }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
     }
 }
