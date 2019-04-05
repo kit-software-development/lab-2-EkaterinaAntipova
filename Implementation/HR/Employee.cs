1f8b0d1bd9f614505ec8e74d16c0d00001f71625
EkaterinaAntipova <ekaterina.antipova.99@mail.ru>
2019-04-05 22:23:43 +0300

    Update Employee.cs

--- Implementation/HR/Employee.cs
+++ Implementation/HR/Employee.cs
@@ -1,12 +1,35 @@
-﻿namespace Practice.HR
+using System;
+using Practice.HR.Events;
+using Practice.Organization;
+
+namespace Practice.HR
 {
     /// <summary>
     ///     Скрытая реализация представления о сотруднике.
     /// </summary>
-    internal class Employee : AbstractPerson
+    internal class Employee : AbstractPerson, IEmployee
     {
         /*
          * TODO #5: Реализуйте интерфейс IEmployee для класса Employee
          */
+
+	private IDepartment o_department;
+
+        public IDepartment Department
+        {
+            get { return o_department; }
+
+            set
+            {
+                var oldDepartment = o_department;
+                o_department = value;
+                if (DepartmentChange != null)
+                {
+                    DepartmentChange.Invoke(this, new ValueChangeEventArgs<IDepartment>(oldDepartment));
+                }
+            }
+        }
+
+        public event EventHandler<ValueChangeEventArgs<IDepartment>> DepartmentChange;
     }
 }
