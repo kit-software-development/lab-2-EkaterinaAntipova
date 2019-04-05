102241c5d7e90de3e65fc06fca1909b4055e27c7
EkaterinaAntipova <ekaterina.antipova.99@mail.ru>
2019-04-05 22:16:59 +0300

    Update AbstractPerson.cs

--- Implementation/HR/AbstractPerson.cs
+++ Implementation/HR/AbstractPerson.cs
@@ -1,39 +1,37 @@
-﻿namespace Practice.HR
+using Practice.Common;
+using Practice.HR.Events;
+using System;
+
+namespace Practice.HR
 {
     /// <summary>
     ///     Абстрактная база для описания конкретных реализаций типа "Человек".
     ///     Используется для дальнейшего наследования.
     /// </summary>
-    internal abstract class AbstractPerson
+    internal abstract class AbstractPerson : IPerson
     {
-        internal Organization.Department Department
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-
-            set
-            {
-                throw new System.NotImplementedException();
-            }
-        }
-
-        internal Common.Name Name
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-
-            set
-            {
-                throw new System.NotImplementedException();
-            }
-        }
         
         /*
          * TODO #3: Реализуйте интерфейс IPerson для класса AbstractPerson
          */
+
+	private IName o_name;
+
+        public IName Name
+        {
+            get { return o_name; }
+            set
+            {
+                throw new System.NotImplementedException();
+                var old_name = o_name;
+                o_name = value;
+                if (NameChange != null) 
+                {
+                    NameChange.Invoke(this, new ValueChangeEventArgs<IName>(old_name));
+                }
+            }
+        }	
+
+	public event EventHandler<ValueChangeEventArgs<IName>> NameChange;
     }
 }
